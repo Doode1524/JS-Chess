19c26bc934ba74b6f6d8981ee236f1ae4fc09e98
Joe@Edood <Doode1524@gmail.com>
2021-02-22 23:01:56 -0500

    add move piece function (blue bug?)

--- frontend/chess.js
+++ frontend/chess.js
@@ -1,37 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 })
+
 let currentPiece;
+let copy;
 let phase = 0 // zero is selected, 1 is moving
-
-let wk = document.getElementById('b1')
-let a3 = document.getElementById('a3')
-
-wk.addEventListener("click", function(e) {
-    wk.style.color = 'green'
-    a3.addEventListener('click', function(e) {
-
-            a3.innerText = b1.innerText
-            a3.style.color = 'red'
-            b1.innerText = ""
-        })
-        // e.dataTransfer.setData("Text", e.target.id)
-        // wk.innerText = ""
-});
 
 //// LOOK INTO BUBBLING ///// 
 
 let board = document.getElementById('chessboard')
+
 board.addEventListener('click', handleClick)
 
 function handleClick(e) {
     console.log(e.target, "was clicked!")
     let selectedDiv = e.target
-    toggleSelected(selectedDiv)
-    console.log(phase)
-
+    if (phase == 0 || selectedDiv == currentPiece) {
+        toggleSelected(selectedDiv)
+        console.log(phase)
+    } else {
+        selectedDiv.innerText = copy
+        currentPiece.innerText = ""
+        phase = 0
+        console.log(phase)
+    } // check else if
 }
-
 
 function toggleSelected(selectedDiv) {
     if (selectedDiv == currentPiece) {
@@ -40,22 +33,8 @@ function toggleSelected(selectedDiv) {
         phase = 0
     } else {
         currentPiece = selectedDiv
+        copy = currentPiece.innerText
         selectedDiv.style.color = "blue"
         phase = 1
     }
 }
-
-function move(currentPiece) {
-    if (phase == 1) {
-        selectedDiv.innerText = currentPiece.innerText
-    }
-}
-// if there is current piece
-// delete piece in move phase
-// if not in moving phase
-// there is no currentPiece
-
-
-// make another click event
-// attach it to nodes
-// move function
