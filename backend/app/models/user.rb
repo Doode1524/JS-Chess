58094d5549cc55c19a3a238e10f1caa21281e510
Joe@Edood <Doode1524@gmail.com>
2021-03-10 19:30:56 -0500

    update user model to track games better

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,8 @@
-class User < ApplicationRecord
-    has_many :messages
-    has_many :games
+class User < ActiveRecord::Base
+    has_many :white_player_games, class_name: "Game", foreign_key: "white_player_id"
+    has_many :black_player_games, class_name: "Game", foreign_key: "black_player_id"
 end
+
+
+  
+ 
